e62e68738852b057f9f2d101b11837dbcf124e74
Sig L <sig.lange@gmail.com>
2014-05-14 16:21:19 -0700

    make an Application class

--- screenshot/cli.py
+++ screenshot/cli.py
@@ -15,37 +15,46 @@ def configure_logging(level):
    rootlog.addHandler(stderr_handler)
    rootlog.setLevel(getattr(logging, level))
 
+class Application:
+
+    def __init__(self):
+       self.log = logging.getLogger(self.__class__.__name__)
+
+    def main(self):
+
+       parser = OptionParser()
+       parser.add_option("-c", "--config", dest="config", help="config file", metavar="FILE", default=None)
+       parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False)
+       parser.add_option("-l", "--log-level", dest="log_level", default='info', help="set logging level [%default]")
+       parser = parser
+
+       (options, args) = parser.parse_args()
+
+       configure_logging(options.log_level)
+
+       # Mute boto unless we're verbose
+       if options.verbose == False:
+            logging.getLogger('boto').setLevel(logging.CRITICAL)
+            logging.getLogger('requests').setLevel(logging.CRITICAL)
+
+
+       opts = config.GetScreenshotOptions(options.config)
+       for k, v in opts.__dict__.iteritems():
+            self.log.debug("%s: %s", k, v)
+       app = Screenshot(opts)
+
+       app.configure()
+
+
+
+       shortname = None
+       if len(args) > 0:
+        shortname = args[0]
+
+       app.take_screenshot(shortname)
+
 def main():
-
-   parser = OptionParser()
-   parser.add_option("-c", "--config", dest="config", help="config file", metavar="FILE", default=None)
-   parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False)
-   parser.add_option("-l", "--log-level", dest="log_level", default='info', help="set logging level [%default]")
-   parser = parser
-
-   (options, args) = parser.parse_args()
-
-   configure_logging(options.log_level)
-
-   # Mute boto unless we're verbose
-   if options.verbose == False:
-        logging.getLogger('boto').setLevel(logging.CRITICAL)
-        logging.getLogger('requests').setLevel(logging.CRITICAL)
-
-
-   opts = config.GetScreenshotOptions(options.config)
-
-   app = Screenshot(opts)
-
-   app.configure()
-
-
-
-   shortname = None
-   if len(args) > 0:
-    shortname = args[0]
-
-   app.take_screenshot(shortname)
+    Application().main()
 
 if __name__ == '__main__':
-   main()
+    main()
