899dbcd8892feba64dae8f01d2181335e65c6fc5
Sig Lange <sig.lange@gmail.com>
2015-07-27 16:46:56 -0700

    remove zip_safe

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
    version='1.0.0', 
    packages=find_packages('.'),
    install_requires=install_requires,
+   zip_safe=False,
    entry_points = {
     'console_scripts': [
         'screenshot = screenshot.cli:main',
