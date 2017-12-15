39fb44ce492403e6e359040b5b89b9be11932ae4
sig <sig.lange@gmail.com>
2017-12-14 17:00:33 -0800

    fix imgur uploader

--- screenshot/upload/imgur.py
+++ screenshot/upload/imgur.py
@@ -1,20 +1,16 @@
 """imgur screenshot saver"""
 try:
-   from pyimgur import Imgur
-except:
-   Imgur = None
-
+   from pyimgur import Imgur as PyImgur
+except Exception, e:
+   PyImgur = None
 from screenshot.upload import UploadPlugin
 
 class Imgur(UploadPlugin):
-
    upload_method = 'imgur'
-
    def upload(self, meta, filename, shortname, md):
-      imgur = Imgur(
-         self.config['client_id'],
-         self.config['client_secret']
-      )
-      result = imgur.upload_image(filename, title=shortname)
+      client_id = self.config['client_id']
+      client_secret = self.config['client_secret']
+      up = PyImgur(client_id, client_secret)
+      result = up.upload_image(filename, title=shortname)
       self.set_url('http://imgur.com/v/'+result.id)
       return True
